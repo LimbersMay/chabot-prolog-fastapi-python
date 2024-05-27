cfee804ed621b16acdb9f1d4fbb5521ef7f7bfea
LimbersMay <limbermay1@gmail.com>
2024-05-26 23:44:43 -0600

    Refactor Habitos model in server.py for better organization

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ from prolog import procesar_habitos
 app = FastAPI()
 
 class Habitos(BaseModel):
-    habitos_usuario: list
+    objetivos: list[str]
+    problemas_salud: list[str]
+    habitos_perjudiciales: list[str]
+    habitos: list[str]
 
 
 @app.get("/")
@@ -16,4 +19,11 @@ async def root():
 
 @app.post("/habitos")
 async def say_hello(habitos_usuario: Habitos):
-    return procesar_habitos(habitos_usuario.habitos_usuario)
+
+    habitos = []
+    habitos.extend(habitos_usuario.objetivos)
+    habitos.extend(habitos_usuario.problemas_salud)
+    habitos.extend(habitos_usuario.habitos_perjudiciales)
+    habitos.extend(habitos_usuario.habitos)
+
+    return procesar_habitos(habitos)
