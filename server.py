f6a1f505665e6a41b4d2fdc138121e4814bbc9b1
LimbersMay <limbermay1@gmail.com>
2024-05-27 10:05:01 -0600

    Configuración de cors agregado para permitir al cliente realizar peticiones

--- server.py
+++ server.py
@@ -2,8 +2,29 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from prolog import procesar_habitos
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
 
 class Habitos(BaseModel):
     objetivos: list[str]
